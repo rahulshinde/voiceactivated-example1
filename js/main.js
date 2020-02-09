33abf771f8a7ad3975b775dd8444c04da87eaa35
Rahul S. Shinde <rshinde@mica.edu>
2020-02-09 10:50:35 -0500

    new js file using annyang

--- js/main.js
+++ js/main.js
@@ -32,22 +32,13 @@ Site.rows = {
 
 Site.columns = {
   'one': '1',
-  '1': '1',
   'two': '2',
-  '2': '2',
   'three': '3',
-  '3': '3',
   'four': '4',
-  'for': '4',
-  '4': '4',
   'five': '5',
-  '5': '5',
   'six': '6',
-  '6': '6',
   'seven': '7',
-  '7': '7',
-  'eight': '8',
-  '8': '8',
+  'eight': '8'
 }
 
 recognition.onresult = function (event) {
